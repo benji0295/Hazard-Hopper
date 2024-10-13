7a4432db83a979f8ea10f8a69fc838ce570a0108
Ben Smith <ben.c.smith95@gmail.com>
2024-10-13 09:52:55 -0400

    Added sprites to main menu that run on a loop

--- Assets/Scripts/AIMenu.cs
+++ Assets/Scripts/AIMenu.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class AIMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  public float speed = 4.0f;
+  public float rightLimit = 20.0f;
+  public LayerMask groundLayer;
 
-    // Update is called once per frame
-    void Update()
+  private Rigidbody2D playerRigidbody;
+  private Rigidbody2D sawRigidbody;
+  private Vector2 playerStartingPosition;
+  private Vector2 sawStartingPosition;
+  private GameObject player;
+  private GameObject saw;
+
+  private void Start()
+  {
+    playerRigidbody = GetComponent<Rigidbody2D>();
+    sawRigidbody = GetComponent<Rigidbody2D>();
+    player = GameObject.FindGameObjectWithTag("Player");
+    saw = GameObject.FindGameObjectWithTag("Enemy");
+    playerStartingPosition = player.transform.position;
+    sawStartingPosition = saw.transform.position;
+
+  }
+
+  private void Update()
+  {
+    Vector2 playerVelocity = playerRigidbody.velocity;
+    Vector2 sawVelocity = sawRigidbody.velocity;
+
+    playerVelocity.x = speed;
+    sawVelocity.x = speed;
+
+    playerRigidbody.velocity = playerVelocity;
+    sawRigidbody.velocity = sawVelocity;
+
+    if (player.transform.position.x >= rightLimit || saw.transform.position.x >= rightLimit)
     {
-        
+      player.transform.position = playerStartingPosition;
+      saw.transform.position = sawStartingPosition;
     }
+  }
 }
